e33271fc881a8507622501edc7ba10fcb3d507f6
AbelMuro <abelmuro93@gmail.com>
2024-08-21 15:55:12 -0700

    commit

--- src/Components/Nutrition/Nutrition.js
+++ src/Components/Nutrition/Nutrition.js
@@ -3,9 +3,52 @@ import * as styles from './styles.module.css';
 
 function Nutrition(){
     return(
-        <>
-        
-        </>
+        <section className={styles.container}>
+            <h1>
+                Nutrition
+            </h1>
+            <p>
+                The table below shows nutritional 
+                values per serving without the 
+                additional fillings.
+            </p>
+            <table className={styles.table}>
+                <tbody>
+                    <tr>
+                        <td className={styles.table_header}>
+                            Calories
+                        </td>
+                        <td className={styles.table_data}>
+                            277kcal
+                        </td>                        
+                    </tr>
+                    <tr>
+                        <td className={styles.table_header}>
+                            Carbs
+                        </td>
+                        <td className={styles.table_data}>
+                            0g
+                        </td>                        
+                    </tr>
+                    <tr>
+                        <td className={styles.table_header}>
+                            Protein
+                        </td>
+                        <td className={styles.table_data}>
+                            20g
+                        </td>                        
+                    </tr>
+                    <tr>
+                        <td className={styles.table_header}>
+                            Fat
+                        </td>
+                        <td className={styles.table_data}>
+                            22g
+                        </td>                        
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     )
 }
 
